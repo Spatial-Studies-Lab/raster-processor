8bc82268c2ec192d9f9729cced4de8f4440c5468
David Heyman <dave@axismaps.com>
2020-11-20 10:57:02 +0000

    Error handling

--- prep_rasters.py
+++ prep_rasters.py
@@ -14,40 +14,44 @@ s3 = boto3.client('s3')
 def raster_bands(tif, sub):
   tif_file = PATH + sub + tif
   print('Reading raster', tif_file)
-  src = gdal.Open(tif_file)
-  ras = rasterio.open(tif_file)
-
-  val = ras.read(1)[0][0]
-
-  if (src.RasterCount == 4 or (val != 0 and val != 255)) and ras.dtypes[0] == 'uint8':
-    print('4 correct bands found')
-    copyfile(tif_file, PATH + 'converted/' + tif)
-    project_raster(tif)
-  elif src.RasterCount == 1 or src.RasterCount == 3 or src.RasterCount == 4:
-    gdal_string = Template("""gdal_translate -b 1 ${f} -a_nodata none ${path}converted/red.tif &&
-        gdal_translate -b ${b2} ${f} -a_nodata none ${path}converted/green.tif &&
-        gdal_translate -b ${b3} ${f} -a_nodata none ${path}converted/blue.tif &&
-        echo Calculating file mask
-        gdal_calc.py -A ${path}converted/red.tif -B ${path}converted/green.tif -C ${path}converted/blue.tif --outfile=${path}converted/mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
-        echo Merging files
-        gdal_merge.py -separate -ot Byte -o ${path}converted/${tif} ${path}converted/red.tif ${path}converted/green.tif ${path}converted/blue.tif ${path}converted/mask.tif &&
-        echo Cleaning up
-        rm ${path}converted/red.tif &&
-        rm ${path}converted/green.tif &&
-        rm ${path}converted/blue.tif &&
-        rm ${path}converted/mask.tif""")
-    if src.RasterCount == 1:
-      print('1 band raster found')
-      os.system(gdal_string.substitute(f=tif_file, b2='1', b3='1', nodata=str(val),
-                                        path=PATH, tif=tif))
-      project_raster(tif)
-    elif src.RasterCount >= 3:
-      print('3+ band raster found')
-      os.system(gdal_string.substitute(f=tif_file, b2='2', b3='3', nodata=str(val),
-                                        path=PATH, tif=tif))
-      project_raster(tif)
+  try:
+    src = gdal.Open(tif_file)
+    ras = rasterio.open(tif_file)
+  except:
+    print('Cannot read', tif_file)
+  
   else:
-    print(tif_file + ' has wrong number of bands!')
+    val = ras.read(1)[0][0]
+
+    if (src.RasterCount == 4 or (val != 0 and val != 255)) and ras.dtypes[0] == 'uint8':
+      print('4 correct bands found')
+      copyfile(tif_file, PATH + 'converted/' + tif)
+      project_raster(tif)
+    elif src.RasterCount == 1 or src.RasterCount == 3 or src.RasterCount == 4:
+      gdal_string = Template("""gdal_translate -b 1 ${f} -a_nodata none ${path}converted/red.tif &&
+          gdal_translate -b ${b2} ${f} -a_nodata none ${path}converted/green.tif &&
+          gdal_translate -b ${b3} ${f} -a_nodata none ${path}converted/blue.tif &&
+          echo Calculating file mask
+          gdal_calc.py -A ${path}converted/red.tif -B ${path}converted/green.tif -C ${path}converted/blue.tif --outfile=${path}converted/mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
+          echo Merging files
+          gdal_merge.py -separate -ot Byte -o ${path}converted/${tif} ${path}converted/red.tif ${path}converted/green.tif ${path}converted/blue.tif ${path}converted/mask.tif &&
+          echo Cleaning up
+          rm ${path}converted/red.tif &&
+          rm ${path}converted/green.tif &&
+          rm ${path}converted/blue.tif &&
+          rm ${path}converted/mask.tif""")
+      if src.RasterCount == 1:
+        print('1 band raster found')
+        os.system(gdal_string.substitute(f=tif_file, b2='1', b3='1', nodata=str(val),
+                                          path=PATH, tif=tif))
+        project_raster(tif)
+      elif src.RasterCount >= 3:
+        print('3+ band raster found')
+        os.system(gdal_string.substitute(f=tif_file, b2='2', b3='3', nodata=str(val),
+                                          path=PATH, tif=tif))
+        project_raster(tif)
+    else:
+      print(tif_file + ' has wrong number of bands!')
 
 def project_raster(tif):
   mb_string = Template("""gdal2tiles.py -s "EPSG:4326" -z "9-12"\
